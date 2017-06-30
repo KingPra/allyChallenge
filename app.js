b6eff224e3157008e8794de73ff83d195d9c4a96
KingPra <kingi.pra@gmail.com>
2017-06-30 13:57:23 -0400

    login box working

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 console.log('well hello there');
+window.addEventListener('load', function () {
+  login();
+  cancel();
+});
 
-document.getElementsByClassName('bold');
+function login () {
+  let login = document.querySelector('.login_box');
+  let cover = document.querySelector('.cover');
+  if (login.style.display === 'none') {
+    login.style.display = 'block';
+    cover.style.display = 'block'
+  } else {
+    login.style.display = 'none';
+    cover.style.display = 'none';
+  }
+};
+
+  function cancel () {
+    let login = document.querySelector('.login_box');
+    let cover = document.querySelector('.cover');
+    login.style.display = 'none';
+    cover.style.display = 'none';
+  };
